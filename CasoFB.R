f8fcde7ea40e295254526d11915eefcc7cae723e
mcj-data <75705885+mcj-data@users.noreply.github.com>
2022-06-03 22:11:14 -0500

    Update CasoFB.R
    comentario

--- CasoFB.R
+++ CasoFB.R
@@ -3,7 +3,7 @@ plot(FBCSV, main='FB',  xlab='Apertura', ylab='Cierrre')
 plot(FBCSV$Edad, FBCSV$Minutos, main='Paridad Dolar', pch=21, bg="blue", cex=2, xlab='Edad', ylab='Minutos')
 summary(fb)
 plot(fb$Edad, fb$Likes, main='Paridad Dolar', pch=21, bg="blue", cex=2, xlab='Edad', ylab='Likes')
-
+#grafica los datos
 
 ggplot(data=FBCSV) + 
   aes(x=Edad, y=Minutos, color=Name, size=Likes) +
