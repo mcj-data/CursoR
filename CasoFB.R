176fd7e06abad533bc338e9491022951d51e1f0b
mcj-data <75705885+mcj-data@users.noreply.github.com>
2022-06-03 22:09:27 -0500

    Add files via upload

---
+++ CasoFB.R
@@ -0,0 +1,11 @@
+fb=read.csv("FBCSV.csv")
+plot(FBCSV, main='FB',  xlab='Apertura', ylab='Cierrre')
+plot(FBCSV$Edad, FBCSV$Minutos, main='Paridad Dolar', pch=21, bg="blue", cex=2, xlab='Edad', ylab='Minutos')
+summary(fb)
+plot(fb$Edad, fb$Likes, main='Paridad Dolar', pch=21, bg="blue", cex=2, xlab='Edad', ylab='Likes')
+
+
+ggplot(data=FBCSV) + 
+  aes(x=Edad, y=Minutos, color=Name, size=Likes) +
+  geom_point() +
+  geom_smooth(method=lm)
